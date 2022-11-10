8d6c0889c20f052aad92532a74f490be8eba31b8
Jason Kozik <kozikjason@gmail.com>
2022-11-10 11:52:38 -0600

    Completed Task One. Set up the SQL connection and checked the column names and types.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,9 @@
 -- Part 1: Test it with SQL
+--Columns
+--id with the type int, it is a primary key
+--employer with the type varchar(255)
+--name with the type varchar(255)
+--skills with the type varchar(255)
 
 -- Part 2: Test it with SQL
 
