99879d8247eae5fff6bdf1c0c40cf8e92b465b58
Jason Kozik <kozikjason@gmail.com>
2022-11-10 20:05:50 -0600

    Completed tasks 3 and 4. TechJobs is up and running

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,8 +1,11 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -11,6 +14,10 @@ public class Skill extends AbstractEntity {
     @NotBlank(message = "Job description cannot be left blank")
     @Size(min =1, max = 750, message = "Description is limited to 750 characters")
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
 
     public Skill() {
 
@@ -23,4 +30,10 @@ public class Skill extends AbstractEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+
 }
